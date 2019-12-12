f154fa0e976264b071d334a9eb6a0f30ba19bfaa
Mark Gaudiano <markgaudiano@Marks-iMac.local>
2019-12-12 13:31:59 +0800

    add rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
+const slugify = require("slugify")
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby + NetlifyCMS`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://www.example.com`,
+    siteUrl: `https://webriq-gatsby-cms-starter-template-netlify.webriq.me`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,6 +23,63 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node, {
+                  description: edge.node.frontmatter.excerpt,
+                  date: edge.node.frontmatter.publishedDate,
+                  url:
+                    site.siteMetadata.siteUrl +
+                    "/" +
+                    slugify(edge.node.frontmatter.title).toLowerCase(),
+                  guid:
+                    site.siteMetadata.siteUrl +
+                    "/" +
+                    slugify(edge.node.frontmatter.title).toLowerCase(),
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/posts/"}}) {
+                  edges {
+                    node {
+                      id
+                      html
+                      frontmatter {
+                        excerpt
+                        publishedDate
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Breglio Long RSS",
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
